c3313d16529e857ba0c955b446a7ed5208977ca0
asafwat <bedo_aboraia@yahoo.com>
2019-10-26 15:41:51 +0200

    Project done

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get('/api/whoami', (req, res) => {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"]
+    });
+});
+
+app.listen(process.env.PORT | 3000);
